ce4b2cb4e7045fe2c42d6fb619b48333a85331af
nenad <nenad@DESKTOP-PTV5NND>
2025-02-15 16:25:23 +0100

    Izmjene

--- src/app/admin/tenant/tenant.service.ts
+++ src/app/admin/tenant/tenant.service.ts
@@ -5,11 +5,15 @@ import { Tenant } from "./tenant.model";
 
 @Injectable({ providedIn: 'root' })
 export class TenantService {
-    private apiUrl = 'https://localhost:7065/api/tenants';
+  private apiUrl = 'http://must-soft.runasp.net/admin/tenants';
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
     
-    getTenants(): Observable<Tenant[]> {
-        return this.http.get<Tenant[]>(this.apiUrl);
-     }
+  getTenants(): Observable<Tenant[]> {
+    return this.http.get<Tenant[]>(this.apiUrl);
+  }
+
+  addTenant(tenant: Tenant): Observable<Tenant> {
+    return this.http.post<Tenant>(this.apiUrl, tenant);
+  }
 }
